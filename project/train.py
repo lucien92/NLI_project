199178d442139573da53b871c9fed243bbc8e502
Kreyparion <kreyparion@gmail.com>
2024-03-26 13:35:17 +0100

    fix training

--- project/train.py
+++ project/train.py
@@ -5,34 +5,31 @@ import pandas as pd
 import random
 import yaml
 from model import allMiniLMModel
-
-from datasets import load_dataset
-snli = load_dataset("snli")
-
-train_dataset = load_dataset("snli", split='train')
-test_dataset = load_dataset("snli", split='test')
-validation_dataset = load_dataset("snli", split='validation')
-
 from data_utils import PreProcessor, Filter
-filter = Filter()
-preprocessor = PreProcessor()
-
-train_dataset_filtered = filter.transform(train_dataset)
-test_dataset_filtered = filter.transform(test_dataset)
-validation_dataset_filtered = filter.transform(validation_dataset)
-
-train_dataset_processed, labels_train = preprocessor.fit_transform(train_dataset_filtered) 
-test_dataset_processed, labels_test = preprocessor.transform(test_dataset_filtered)
-validation_dataset_processed, labels_val = preprocessor.transform(validation_dataset_filtered)
+from datasets import load_dataset
 
 if __name__ == "__main__":
+    
+    snli = load_dataset("snli")
 
-    train_dataset=train_dataset_processed
-    valid_dataset=validation_dataset_processed
-    test_dataset=test_dataset_processed
+    train_dataset = load_dataset("snli", split='train')
+    test_dataset = load_dataset("snli", split='test')
+    validation_dataset = load_dataset("snli", split='validation')
+
+
+    filter = Filter()
+    preprocessor = PreProcessor()
+
+    train_dataset_filtered = filter.transform(train_dataset)
+    test_dataset_filtered = filter.transform(test_dataset)
+    validation_dataset_filtered = filter.transform(validation_dataset)
+
+    train_dataset_processed, labels_train = preprocessor.fit_transform(train_dataset_filtered) 
+    test_dataset_processed, labels_test = preprocessor.transform(test_dataset_filtered)
+    validation_dataset_processed, labels_val = preprocessor.transform(validation_dataset_filtered)
 
     config = yaml.safe_load(open("project/config.yml", "r"))
-    training_config = config["training"]
+    training_config = config["training_config"]
 
     model_name=training_config["model_name"] 
     num_labels=training_config["num_labels"] 
@@ -49,4 +46,4 @@ if __name__ == "__main__":
     wandb_project_name=config_wandb["project"]
     wandb_entity = config_wandb["entity"]
 
-    model = allMiniLMModel(model_name, num_labels, output_dir, train_dataset, valid_dataset, test_dataset, batch_size, epochs, learning_rate, seed, warmup_steps,wandb_project_name=None, wandb_api_key=None)
+    model = allMiniLMModel(model_name, num_labels, output_dir, train_dataset_processed, validation_dataset_processed, test_dataset_processed, batch_size, epochs, learning_rate, seed, warmup_steps,wandb_project_name=None, wandb_api_key=None)
